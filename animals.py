a2ed1c63d3215ac3229cf0f0f746999dc9b797e5
avi-shrestha <avishekh.shrestha@fuse.ai>
2020-02-17 16:15:19 +0545

    Added Dog functionality

--- animals.py
+++ animals.py
@@ -6,9 +6,14 @@ def default():
 def cat():
     print('Meow!')
 
+def dog():
+    print('Bark!')
+
 def main():
     if sys.argv[1] == 'cat':
         cat()
+    elif sys.argv[1] == 'dog':
+        dog()
     else:
         default()
 
