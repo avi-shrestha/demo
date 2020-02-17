c4105cb6a8b3ceb3d4fd3c22b0650c74cdc08807
avi-shrestha <avishekh.shrestha@fuse.ai>
2020-02-17 16:12:04 +0545

    Added Cat functionality

---
+++ animals.py
@@ -0,0 +1,16 @@
+import sys
+
+def default():
+    print("Hello World")
+
+def cat():
+    print('Meow!')
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
+
+if __name__ == '__main__':
+    main()
